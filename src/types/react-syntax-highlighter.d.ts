9da703eb58d439535d792a94532ebdbf1fbc14bc
Fiona Przybylski <fionaprzybylski@fionas-mbp.home>
2025-12-05 10:33:54 +0000

    install and set up react-syntax-highlighter

---
+++ src/types/react-syntax-highlighter.d.ts
@@ -0,0 +1,16 @@
+declare module 'react-syntax-highlighter' {
+  const content: any;
+  export default content;
+  export const Prism: any;
+  export const Light: any;
+}
+
+declare module 'react-syntax-highlighter/dist/esm/styles/prism/*' {
+  const content: any;
+  export default content;
+}
+
+declare module 'react-syntax-highlighter/dist/esm/styles/hljs/*' {
+  const content: any;
+  export default content;
+}
